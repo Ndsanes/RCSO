182ccbf3d2f18936fbba71e851d64c35754d4a7e
NDsanes <nd@m.ngames.work>
2024-09-10 21:29:10 +0800

    处理了没有配置文件的情况 处理了配置文件为默认文件的情况 处理了rclone.exe不存在的情况

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,32 @@ use regex::Regex;
 use std::io;
 use std::io::Read;
 use std::io::{BufReader, Stdout, Write};
-use std::process::{Command, Stdio};
+use std::process::{exit, Command, Stdio};
 
 fn main() {
+    println!("正在开始同步……");
     // 读取配置文件
+    if !std::path::Path::new("setting.toml").exists() {
+        eprintln!("配置文件不存在！尝试新建一个 setting.toml 文件……");
+        let file = std::fs::File::create("setting.toml").unwrap();
+        let mut writer = io::BufWriter::new(file);
+        // 写入默认配置
+        let _ = writer.write_all(
+            br#"
+            [sync_file]
+            local_path = "your local path"
+            remote_path = "your remote path,default like onedrive:/the/path/you/want/to/sync"
+            
+            [lark]
+            webhook = "https://open.feishu.cn/open-apis/bot/v2/hook/xxxxxx"
+            "#
+        );
+        println!("配置文件已生成，请修改配置文件后重新运行程序！");
+        exit(1);
+    }
+    else { 
+        println!("配置文件存在，正在读取配置文件……"); }
+    
     let settings = Config::builder()
         .add_source(File::with_name(r"setting.toml").format(config::FileFormat::Toml))
         .build()
@@ -14,7 +36,15 @@ fn main() {
     let local_path = settings.get_string("sync_file.local_path").unwrap();
     let remote_path = settings.get_string("sync_file.remote_path").unwrap();
     let lark_webhook = settings.get_string("lark.webhook").unwrap();
-
+    
+    if local_path == "your local path" || remote_path == "your remote path,default like onedrive:/the/path/you/want/to/sync" || lark_webhook == "https://open.feishu.cn/open-apis/bot/v2/hook/xxxxxx" {
+        eprintln!("请修改配置文件后重新运行程序！");
+        exit(2);
+    }
+    else if !std::path::Path::new("rclone.exe").exists() {
+        eprintln!("rclone.exe不存在！请将rclone.exe放在程序同目录下！");
+        exit(3);
+    }
     // 启动子进程
     let mut child = Command::new("./rclone.exe")
         .arg("sync")
