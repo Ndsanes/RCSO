c38f3d2ef88bd2a7bb7d16e5ccdf09f90c2ded9d
NDsanes <nd@m.ngames.work>
2024-09-11 22:53:11 +0800

    重构了消息匹配的相关代码 提示信息嘴臭能力+++

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,50 @@
 use config::{Config, File};
 use regex::Regex;
-use std::io;
-use std::io::Read;
-use std::io::{BufReader, Stdout, Write};
+use std::io::{self, BufReader, Read, Write};
 use std::process::{exit, Command, Stdio};
 
 fn main() {
     println!("正在开始同步……");
-    // 读取配置文件
+    // 检查配置文件是否存在
     if !std::path::Path::new("setting.toml").exists() {
-        eprintln!("配置文件不存在！尝试新建一个 setting.toml 文件……");
-        let file = std::fs::File::create("setting.toml").unwrap();
-        let mut writer = io::BufWriter::new(file);
-        // 写入默认配置
-        let _ = writer.write_all(
+        println!("哎我草！配置文件不存在！等等嗷，老子尝试新建一个 setting.toml 文件……");
+        std::fs::write(
+            "setting.toml",
             br#"
             [sync_file]
             local_path = "your local path"
             remote_path = "your remote path,default like onedrive:/the/path/you/want/to/sync"
-            
+
             [lark]
             webhook = "https://open.feishu.cn/open-apis/bot/v2/hook/xxxxxx"
-            "#
-        );
-        println!("配置文件已生成，请修改配置文件后重新运行程序！");
+            "#,
+        ).unwrap();
+        println!("嘀嘀嘀！！！配置文件已生成，请修改配置文件后重新运行程序！！！");
+        pause().unwrap();
         exit(1);
+    } else {
+        println!("配置文件存在，正在读取配置文件……");
     }
-    else { 
-        println!("配置文件存在，正在读取配置文件……"); }
-    
+    // 读取配置文件
     let settings = Config::builder()
-        .add_source(File::with_name(r"setting.toml").format(config::FileFormat::Toml))
+        .add_source(File::with_name("setting.toml").format(config::FileFormat::Toml))
         .build()
         .unwrap();
     let local_path = settings.get_string("sync_file.local_path").unwrap();
     let remote_path = settings.get_string("sync_file.remote_path").unwrap();
     let lark_webhook = settings.get_string("lark.webhook").unwrap();
-    
+
     if local_path == "your local path" || remote_path == "your remote path,default like onedrive:/the/path/you/want/to/sync" || lark_webhook == "https://open.feishu.cn/open-apis/bot/v2/hook/xxxxxx" {
-        eprintln!("请修改配置文件后重新运行程序！");
+        eprintln!("布什哥们，你没改配置文件呢还！请修改配置文件后重新运行程序！");
+        pause().unwrap();
         exit(2);
-    }
-    else if !std::path::Path::new("rclone.exe").exists() {
-        eprintln!("rclone.exe不存在！请将rclone.exe放在程序同目录下！");
+    } else if !std::path::Path::new("rclone.exe").exists() {
+        eprintln!("我草！rclone.exe不存在！麻烦你安一下rclone！安了的话请你把rclone放在本程序所在的目录下！！！");
+        pause().unwrap();
         exit(3);
     }
-    // 启动子进程
+    
+    // 开始同步
     let mut child = Command::new("./rclone.exe")
         .arg("sync")
         .arg(&local_path)
@@ -56,15 +55,15 @@ fn main() {
         .stdout(Stdio::piped())
         .spawn()
         .expect("Failed to execute command");
-
+    
     let stdout = child.stdout.as_mut().expect("Failed to capture stdout");
     let reader = BufReader::new(stdout);
-
+    
     let mut buffer = Vec::new();
     for c in reader.bytes() {
         let c = c.unwrap();
         buffer.push(c);
-        Stdout::write(&mut io::stdout(), &[c]).unwrap();
+        io::stdout().write_all(&[c]).unwrap();
     }
 
     let binding = String::from_utf8(buffer).unwrap();
@@ -72,68 +71,50 @@ fn main() {
 
     let status = child.wait().expect("Failed to wait on child");
     if status.success() {
-        let num_lines = 10; // 输出最后10行
-        let start = if lines.len() > num_lines {
-            lines.len() - num_lines
-        } else {
-            0
-        };
-        let mut last_lines = lines[start..].to_vec();
-        last_lines.reverse(); // 颠倒顺序
-
-        // 定义正则表达式，处理特殊字符
+        // 获取最后10行
+        let num_lines = 10;
+        let start = if lines.len() > num_lines { lines.len() - num_lines } else { 0 };
+        let last_lines = &lines[start..];
+        // 正则匹配
         let re_transferred = Regex::new(r"Transferred:\s+(\d+\.?\d* \w+) / (\d+\.?\d* \w+),").unwrap();
         let re_checks = Regex::new(r"Checks:\s+(\d+) / (\d+), (\d+)%").unwrap();
         let re_deleted = Regex::new(r"Deleted:\s+(\d+) \(files\), (\d+) \(dirs\), (\d+\.?\d* \w+) \(freed\)").unwrap();
         let re_transferred_simple = Regex::new(r"Transferred:\s+(\d+) / (\d+), (\d+)%").unwrap();
         let re_elapsed = Regex::new(r"Elapsed time:\s+(\d+m\d+\.?\d*s)").unwrap();
+        // 匹配结果
+        let mut transferred: Option<String> = None;
+        let mut checks: Option<String> = None;
+        let mut deleted: Option<String> = None;
+        let mut transferred_simple: Option<String> = None;
+        let mut elapsed_time: Option<String> = None;
 
-        // 设置默认值
-        let mut transferred = "未知".to_string();
-        let mut checks = "未知".to_string();
-        let mut deleted = "未知".to_string();
-        let mut transferred_simple = "未知".to_string();
-        let mut elapsed_time = "未知".to_string();
-
-        // 匹配数据
-        for line in &last_lines {
-            if transferred == "未知" {
-                if let Some(caps) = re_transferred.captures(line) {
-                    transferred = format!("传输文件： {} / {},", &caps[1], &caps[2]);
+        for line in last_lines {
+            match (
+                re_transferred.captures(line),
+                re_checks.captures(line),
+                re_deleted.captures(line),
+                re_transferred_simple.captures(line),
+                re_elapsed.captures(line),
+            ) {
+                (Some(caps), _, _, _, _) if transferred.is_none() => {
+                    transferred = Some(format!("传输大小： {} / {},", &caps[1], &caps[2]));
                 }
-            }
-            if checks == "未知" {
-                if let Some(caps) = re_checks.captures(line) {
-                    checks = format!("检查文件： {} / {}, {}%", &caps[1], &caps[2], &caps[3]);
+                (_, Some(caps), _, _, _) if checks.is_none() => {
+                    checks = Some(format!("检查文件： {} / {}, {}%", &caps[1], &caps[2], &caps[3]));
                 }
-            }
-            if deleted == "未知" {
-                if let Some(caps) = re_deleted.captures(line) {
-                    deleted = format!("删除文件： {} (files), {} (dirs), {} (freed)", &caps[1], &caps[2], &caps[3]);
+                (_, _, Some(caps), _, _) if deleted.is_none() => {
+                    deleted = Some(format!("删除文件： {} (files), {} (dirs), {} (freed)", &caps[1], &caps[2], &caps[3]));
                 }
-            }
-            if transferred_simple == "未知" {
-                if let Some(caps) = re_transferred_simple.captures(line) {
-                    transferred_simple = format!("共传输： {} / {}, {}%", &caps[1], &caps[2], &caps[3]);
+                (_, _, _, Some(caps), _) if transferred_simple.is_none() => {
+                    transferred_simple = Some(format!("共传输： {} / {}, {}%", &caps[1], &caps[2], &caps[3]));
                 }
-            }
-            if elapsed_time == "未知" {
-                if let Some(caps) = re_elapsed.captures(line) {
-                    elapsed_time = format!("总耗时： {}", &caps[1]);
+                (_, _, _, _, Some(caps)) if elapsed_time.is_none() => {
+                    elapsed_time = Some(format!("总耗时： {}", &caps[1]));
                 }
+                _ => {}
             }
         }
-
-        // 打印结果
-        println!("=====================");
-        println!("{}", transferred);
-        println!("{}", checks);
-        println!("{}", deleted);
-        println!("{}", transferred_simple);
-        println!("{}", elapsed_time);
-        println!("=====================");
-
-        // 开启powershell脚本子进程，将结果以参数的格式发送到脚本中
+        // 发送消息
         let _output = Command::new("powershell")
             .arg("-Command")
             .arg(format!(
@@ -145,11 +126,26 @@ fn main() {
                     }}
                 }}' {}
                 "#,
-                transferred, checks, deleted, transferred_simple, elapsed_time, lark_webhook
+                transferred.unwrap_or_else(|| "传输大小：无".to_string()),
+                checks.unwrap_or_else(|| "检查：错误".to_string()),
+                deleted.unwrap_or_else(|| "删除文件：无".to_string()),
+                transferred_simple.unwrap_or_else(|| "共传输：未知".to_string()),
+                elapsed_time.unwrap_or_else(|| "未知".to_string()),
+                lark_webhook
             ))
             .output()
             .expect("Failed to execute command");
     } else {
         eprintln!("Command failed with status: {}", status);
     }
+}
+
+// 按任意键继续（）
+fn pause() -> io::Result<()> {
+    let mut stdout = io::stdout();
+    stdout.write_all(b"\xE6\x8C\x89\xE4\xBB\xBB\xE6\x84\x8F\xE9\x94\xAE\xE7\xBB\xA7\xE7\xBB\xAD\xE2\x80\xA6\xE2\x80\xA6")?;
+    stdout.flush()?;
+    let mut stdin = io::stdin();
+    let _ = stdin.read(&mut [0u8])?;
+    Ok(())
 }
